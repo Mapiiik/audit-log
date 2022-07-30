2556c38e14113cc04bfbda952100e6831239631d
Mapiiik <mapik@mapik.net>
2022-07-30 16:41:19 +0000

    Update SerializableEventTrait

--- src/Event/SerializableEventTrait.php
+++ src/Event/SerializableEventTrait.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace AuditLog\Event;
 
@@ -12,9 +13,19 @@ trait SerializableEventTrait
      *
      * @return string
      */
-    public function serialize()
+    public function serialize(): string
     {
         return serialize(get_object_vars($this));
+    }
+
+    /**
+     * Magic method for serializing the Event instance.
+     *
+     * @return array
+     */
+    public function __serialize(): array
+    {
+        return get_object_vars($this);
     }
 
     /**
@@ -23,7 +34,7 @@ trait SerializableEventTrait
      * @param string $data serialized string
      * @return void
      */
-    public function unserialize($data)
+    public function unserialize(string $data): void
     {
         $vars = unserialize($data);
         foreach ($vars as $var => $value) {
@@ -32,20 +43,34 @@ trait SerializableEventTrait
     }
 
     /**
+     * Magic method for unserializing the Event data.
+     *
+     * @param array $data The serialized data
+     * @return void
+     */
+    public function __unserialize(array $data): void
+    {
+        foreach ($data as $var => $value) {
+            $this->{$var} = $value;
+        }
+    }
+
+    /**
      * Returns an array with the basic variables that should be json serialized.
      *
-     * @return array
+     * @return array<string, mixed>
      */
-    protected function basicSerialize()
+    protected function basicSerialize(): array
     {
         return [
             'type' => $this->getEventType(),
             'transaction' => $this->transactionId,
             'primary_key' => $this->id,
+            'display_value' => $this->displayValue,
             'source' => $this->source,
             'parent_source' => $this->parentSource,
             '@timestamp' => $this->timestamp,
-            'meta' => $this->meta
+            'meta' => $this->meta,
         ];
     }
 }
