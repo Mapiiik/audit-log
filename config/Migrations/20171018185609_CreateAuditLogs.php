6bf1aab1b865fd0b94fa05c3a6ddd17b1beb0447
Mapiiik <mapik@mapik.net>
2022-07-30 16:34:18 +0000

    Code cleanup - migrations

--- config/Migrations/20171018185609_CreateAuditLogs.php
+++ config/Migrations/20171018185609_CreateAuditLogs.php
@@ -1,10 +1,20 @@
 <?php
+declare(strict_types=1);
+
 use Migrations\AbstractMigration;
 
 class CreateAuditLogs extends AbstractMigration
 {
     public $autoId = false;
 
+    /**
+     * Up Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-up-method
+     *
+     * @return void
+     */
     public function up()
     {
         $this->table('audit_logs')
@@ -83,8 +93,16 @@ class CreateAuditLogs extends AbstractMigration
             ->create();
     }
 
+    /**
+     * Down Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-down-method
+     *
+     * @return void
+     */
     public function down()
     {
-        $this->dropTable('audit_logs');
+        $this->table('audit_logs')->drop()->save();
     }
 }
